4d1c32a818e1dfb7eb0278b6e96d4f4f4e76a077
Mustafa Binalhag <mbin828@gmail.com>
2023-01-25 13:06:20 -0500

    one route flask api

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello():
+	return jsonify(message="Hello")
+
+if __name__ == '__main__':
+	app.run(debug=True)
